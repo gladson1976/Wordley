dfc00e2e4f184f369ae72a97283edfb30ca2d9ae
Prabhu Thomas <prabhu.thomas@gmail.com>
2023-07-14 14:34:59 +0530

    Version change.. 1.4..

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [revealWordley, setRevealWordley] = useState(false);
   const [hintCount, setHintCount] = useState(0);
   const appTitle = "Wordley";
-  const appVersion = "1.3";
+  const appVersion = "1.4";
 
   const [dialogSettingsOpen, setDialogSettingsOpen] = useState(false);
   const [dialogStatsOpen, setDialogStatsOpen] = useState(false);
